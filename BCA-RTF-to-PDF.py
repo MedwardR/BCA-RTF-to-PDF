60afcceccf199bb896f7adbef39024d582fcef71
Miles Rissler <miles.rissler@gmail.com>
2022-12-02 07:09:22 -0500

    lists files in current directory

--- BCA-RTF-to-PDF.py
+++ BCA-RTF-to-PDF.py
@@ -1,5 +1,14 @@
+import os
 import json
 from fpdf import FPDF
 
+def appendFiles():
+    direc = os.getcwd()
+    files = os.listdir(direc)
+    files = [f for f in files if os.path.isfile(os.path.join(direc, f)) and f[0] != "."]
+    print(files)
+
 print("BCA RTF to PDF Mass Converter and Formatter")
 input("Press enter to start the operation...")
+
+appendFiles()
