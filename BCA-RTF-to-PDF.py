c86678497501a73ac8fef3ad1ac67430e663aace
Miles Rissler <miles.rissler@gmail.com>
2022-12-02 19:25:36 -0500

    Appends test line to rtf docs

--- BCA-RTF-to-PDF.py
+++ BCA-RTF-to-PDF.py
@@ -2,8 +2,7 @@ import os
 import json
 from fpdf import FPDF
 
-print("\nBCA RTF to PDF Mass Converter and Formatter")
-input("Press enter to start the operation...")
+input("\nBCA RTF to PDF Mass Converter and Formatter\nPress enter to start the operation...\n")
 
 direc = os.getcwd()
 files = [f for f in os.listdir(direc) if os.path.isfile(f) and f[0] != "." and os.path.splitext(f)[1].lower() == ".rtf"]
@@ -14,6 +13,15 @@ for fileName in files:
     print("Processing file " + str(i) + "/" + str(len(files)))
     
     # Append text to document
-    file = open(fileName)
-    
+    file = open(fileName, "r")
+    lines = file.read().split("\n")
+    lines[len(lines) - 2] = "### This is a test line ###"
+    newFileContents = ""
+    for line in lines:
+        newFileContents += line + "\n"
     file.close()
+    file = open(fileName, "w")
+    file.write(newFileContents)
+    file.close()
+
+input("\nProcess complete!\nPress any key to exit...")
