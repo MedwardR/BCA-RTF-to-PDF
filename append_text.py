8a91c8af29e1f996ae1020f11ad5a29120401bed
Miles Rissler <miles.rissler@protonmail.com>
2024-10-06 14:40:42 -0400

    adjsutments

--- append_text.py
+++ append_text.py
@@ -9,6 +9,9 @@ if direc[len(direc) - 3] == " ": direc[len(direc) - 3] = ""
 direc = "".join(direc)
 files = [f for f in os.listdir(direc) if ".rtf" in f and f[0] != "." and os.path.splitext(f)[1].lower() == ".rtf"]
 
+from datetime import date    
+today = date.today().isoformat()
+
 i = 0
 for fileName in files:
     i += 1
@@ -16,12 +19,21 @@ for fileName in files:
     
     # Append text to document
     textToAppend = """
-Replace this line with your own text
+    \line
+    \line
+    \line
+    ______________________________          _______________________________          ______________ \line
+    Provider Signature                             Provider Printed Name                          Date/Time \line
+    }}
     """
+    newFooter1 = "{\\footer\pard\\fs20 \\trowd\\trgaph0\cellx5760\cellx11520\pard\intbl\ql Exported from Epic on "
+    newFooter2 = "\cell\pard\intbl\qr Page {\\field{\\fldinst PAGE}} of {\\field{\\fldinst NUMPAGES}}\cell\intbl\\row\line\line}\\trowd\\trgaph0\\trleft0"
+    
     filePath = direc + fileName
     file = open(filePath, "r")
     lines = file.read().split("\n")
     file.close()
+    lines[9] = newFooter1 + today + newFooter2
     lines[len(lines) - 2] = textToAppend
     
     newFileContents = ""
